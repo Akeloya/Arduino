dcf1d7cbcda1f35619fbefbeab85e8405c055296
Maxim <yugov.max@mail.ru>
2019-12-10 00:08:51 +0700

    Add some code and xceed avalon dock manager to MainWindow.xaml

---
+++ ArduinoCore/Pin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoCore
+{
+    /// <summary>
+    /// Implementation base class of Pin
+    /// </summary>
+    public abstract class Pin
+    {
+    }
+}
